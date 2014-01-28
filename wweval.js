8fd6e458d0963ab74102d611df6ac14b1e22d927
Andrew Duthie <aduthie7@gmail.com>
2014-01-27 21:28:06 -0500

    Add JSHint options to circumvent errors

--- wweval.js
+++ wweval.js
@@ -1,4 +1,5 @@
 /*! wweval 1.0.0 | Copyright 2014 Andrew Duthie | MIT License */
+/* jshint evil: true */
 ;(function(global, factory) {
   // Use UMD pattern to expose exported functions
   if (typeof define === 'function' && define.amd) {
